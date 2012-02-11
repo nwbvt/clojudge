c0647ea15eed95b19f8ffc000feea6abd2317947
Nicholas Watkins Brown <nwbrown@gmail.com>
2012-02-10 22:46:32 -0500

    added some doc

--- src/clojudge/core.clj
+++ src/clojudge/core.clj
@@ -9,30 +9,35 @@
   (valid? [result] "Is the result valid?")
   (errors [result] "The errors associated with the result"))
 
-(defn make-judgement
-  "Determines whether or not a given input is valid according to a list of validators"
-  [input validators]
-  (let [results (map #(% input) validators)]
-    {:valid? (every? valid? results)
-     :errors (reduce into (map errors results))}))
-
 (extend-type clojure.lang.IFn
   Judge
     (judge [f input] (f input)))
 
 (extend-type java.lang.Boolean
-  Result
+  Result   
+  ;; booleans can serve as results, they just cannot have error messages
     (valid? [b] b)
     (errors [_] []))
 
 (extend-type java.util.Map
   Result
+  ;; Maps are results as long as they have :valid? and :errors keys
     (valid? [m] (:valid? m))
     (errors [m] (:errors m)))
 
 (extend-type java.util.List
   Result
+  ;; If it is a result, a list simply contains a list of errors
+  ;; An empty list is valid
     (valid? [l] (empty? l))
     (errors [l] l)
   Judge
+  ;; Lists serve as judges by evaluating the input against each member
     (judge [l input] (make-judgement input l)))
+
+(defn- make-judgement
+  "Determines whether or not a given input is valid according to a list of validators"
+  [input validators]
+  (let [results (map #(% input) validators)]
+    {:valid? (every? valid? results)
+     :errors (reduce into (map errors results))}))
